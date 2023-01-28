dba00ad8045d2b8eb7c8f1474f83bf08170e1c51
Brendan <brendan_leahey@brown.edu>
2023-01-28 15:54:39 -0500

    trying to create ingredients list

--- ChatBot/ChatBot.py
+++ ChatBot/ChatBot.py
@@ -1,6 +1,9 @@
 import langchain
+from langchain.llms import OpenAI
 
 class ChatBot:
     """
     A chatbot that uses a language chain to generate responses.
     """
+
+    llm = OpenAI(temperature=0)
