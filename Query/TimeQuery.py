21f6f0dbb33ce9e9903c27deba85ee1a0523a868
joey <joseph_ricciardulli@brown.edu>
2023-01-29 04:32:22 -0500

    start finishing up timequery

--- Query/TimeQuery.py
+++ Query/TimeQuery.py
@@ -9,4 +9,9 @@ class TimeQuery(Query):
     upperlim : int # The upper range of time to search for
 
     def get_query(self) -> str:
-        return f"SELECT * FROM recipes WHERE time BETWEEN {self.lowerlim} AND {self.upperlim}"
+        return f"SELECT * FROM personalizedtable WHERE time BETWEEN {self.lowerlim} AND {self.upperlim}"
+    
+    def create_lists(self) -> None:
+        query = self.get_query()
+        for row in self.cursor.execute(query):
+            
