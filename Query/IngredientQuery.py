d021e580f8c6d0902ba00c9f068b96d970fa9238
Brendan <brendan_leahey@brown.edu>
2023-01-29 02:50:14 -0500

    almost there on ingredient querying

--- Query/IngredientQuery.py
+++ Query/IngredientQuery.py
@@ -1,5 +1,4 @@
 from SQLQuery import Query
-import sqlite3
 
 class IngredientQuery(Query):
     def __init__(self, ingredientlist: list[str]):
@@ -16,25 +15,37 @@ class IngredientQuery(Query):
         for i in self.ingredientlist:
             self.cursor.execute(f"INSERT INTO ingredientlist Values ('{i}')")
         self.commit()
-
-    def get_query(self) -> str:
-        self.create_ing_table()
-        #TODO: make query only select recipes that have all the ingredients
-        #l = self.cursor.execute(f"SELECT * FROM RAW_recipes WHERE ingredients IN (SELECT * FROM ingredientlist)")
-        return """INSERT INTO personalizedtable
-            SELECT * FROM RAW_recipes WHERE ingredients IN (SELECT * FROM ingredientlist)"""
-
-        '''return """INSERT INTO personalizedtable
-            SELECT * FROM RAW_recipes WHERE id IN
-            (SELECT id FROM ingredientlist WHERE ingredient IN (SELECT
-            ingredient FROM ingredientlist))"""'''
+        print("created ingredientlist table")
 
     def create_personalized_table(self) -> None:
-        query = self.get_query()
+        self.create_ing_table()
         self.create_table("personalizedtable",["name","id","minutes",
             "contributor_id","submitted","tags","nutrition","n_steps","steps",
             "description","ingredients","n_ingredients"], ["text", "int",
             "int", "int", "text", "text", "text", "int",
             "text", "text", "text", "int"])
-        self.cursor.execute(query)
+        ingreds = []
+
+
+        #TODO: if this is reused, make it a function
+        counter = 0
+        for row in self.cursor.execute(f"SELECT ingredients FROM RAW_recipes"):
+            if(counter == 5):
+                break
+            print ("new row")
+            l = row[0].strip('[]').split(', ')
+            for ingred in l:
+                if(ingred[0] == "'"):
+                    ingred = ingred[1:]
+                if(ingred[-1] == "'"):
+                    ingred = ingred[:-1]
+                    ingreds.append(ingred)
+            for(i, ingred) in enumerate(ingreds):
+                ingreds[i] = ingred.replace("\'", "")
+            if(set(ingreds).issubset(set(self.ingredientlist))):
+                #TODO: see if we can take out smaller ingredients
+                counter += 1
+                self.cursor.execute(f'INSERT INTO personalizedtable SELECT * FROM RAW_recipes WHERE ingredients LIKE "{row[0]}"')
+        
+        print("created personalizedtable table")
         self.commit()
