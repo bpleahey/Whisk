f56581c7408914e6903587270bc233a42940606f
alexjwick <alexjwick03@gmail.com>
2023-01-29 06:23:41 -0500

    ingredients kept track of

--- client/src/components/subcomponents/IngredientsSearch.jsx
+++ client/src/components/subcomponents/IngredientsSearch.jsx
@@ -1,18 +1,26 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import Select from 'react-select';
 
 class IngredientsSearch extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { apiResponse: "" };
+        this.state = {
+            apiResponse: "",
+            ingredients: JSON.parse(localStorage.getItem('ingredients')) || [],
+            selectedOption: null
+        };
+
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     callAPI() {
         fetch("http://localhost:9000/ingredients")
             .then(res => res.json())
             .then(res => this.setState({ apiResponse: res })
-            .catch(error => console.error(error)));
+                .catch(error => console.error(error)));
 
     }
 
@@ -20,14 +28,44 @@ class IngredientsSearch extends Component {
         this.callAPI();
     }
 
+    componentDidUpdate() {
+        localStorage.setItem('ingredients', JSON.stringify(this.state.ingredients));
+    }
+
+    handleChange = (selectedOption) => {
+        this.setState({ selectedOption });
+    };
+
+    handleSubmit = (event) => {
+        alert("Submitted ingredient " + this.state.selectedOption)
+        event.preventDefault();
+        const { selectedOption, ingredients } = this.state;
+        this.setState({
+            ingredients: [...ingredients, { name: selectedOption.value }],
+            selectedOption: null,
+        });
+    };
+
     render() {
+        const { ingredients, selectedOption } = this.state;
+
         return (
             <div className="IngredientsSearch">
-                <div className="IngredientsSearch-box">
+                <form onSubmit={this.handleSubmit} className="IngredientsSearch-box">
                     <Select
+                        name={"ingredient"}
                         options={this.state.apiResponse}
+                        value={selectedOption}
+                        onChange={this.handleChange}
+                        placeholder="Select an ingredient"
                     />
-                </div>
+                    <button type="submit">Add ingredient</button>
+                </form>
+                <ul>
+                    {ingredients.map((ingredient, index) => (
+                        <li key={index}>{ingredient.name}</li>
+                    ))}
+                </ul>
             </div>
         );
     }
