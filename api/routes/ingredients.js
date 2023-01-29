35a1674672c210905f3a57405de7b9ac6f305698
alexjwick <alexjwick03@gmail.com>
2023-01-29 03:57:28 -0500

    working

--- api/routes/ingredients.js
+++ api/routes/ingredients.js
@@ -1,9 +1,31 @@
 var express = require('express');
+const csv = require('csv-parser');
+const fs = require('fs');
 var router = express.Router();
+
+let ingredients = [];
+
+fs.createReadStream('./data/ingredients.csv')
+    .pipe(csv())
+    .on('data', (data) => {
+        ingredients.push(data);
+    })
+    .on('end', () => {
+        console.log('CSV file successfully processed');
+    });
+
+let ingredientsDropdown = [];
+
+for (let k = 0; k < ingredients.length; k++) {
+    drown.push({
+        "value": ingredientsDropdown[k]['ingredient'],
+        "label": ingredientsDropdown[k]['ingredient']
+    })
+}
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    res.json(ingredients);
+    res.json(ingredientsDropdown);
 });
 
 module.exports = router;
